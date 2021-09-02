74eb7793e62912180ab3fc899952798b2b2f9b95
Soumyaranjan-Rout <routsoumyaranjan2021@gmail.com>
2021-09-02 09:54:28 +0530

    New program added

---
+++ Conditionals-Loops/Intermediate-Programs/Power.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+/**
+ * Power
+ */
+public class Power {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Base:");
+        int baseValue = input.nextInt();
+        System.out.print("Enter Power:");
+        int powerValue = input.nextInt();
+        double result = Math.pow(baseValue, powerValue);
+        System.out.println("So " + baseValue + " to the power " + powerValue + " = " + result);
+        input.close();
+
+    }
+}
