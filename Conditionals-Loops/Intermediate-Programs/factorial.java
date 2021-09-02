5ad4cf3ac6cd6e4fd92cf341deae9178c6715ee2
Soumyaranjan-Rout <routsoumyaranjan2021@gmail.com>
2021-09-02 09:33:52 +0530

    Replacing file to their corresponding directory

--- Conditionals-Loops/Intermediate-Programs/factorial.java
+++ Conditionals-Loops/Intermediate-Programs/factorial.java
@@ -1,25 +1,31 @@
-import java.util.*;
+import java.io.*;
 
 /**
- * factorial
+ ** Write a program to print factorial of a number, also take input.
  */
-public class factorial {
+public class Factorial {
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Enter a  number:");
-        int Number = in.nextInt();
-        int factorial_Number = fact(Number);
-        System.out.println("Factorial of " + Number + " is" + factorial_Number);
-        in.close();
-
-    }
-
-    public static int fact(int number) {
-        if (number == 0) {
-            return 1;
-        } else {
-            return number * fact(number - 1);
+    public static void main(String[] args) throws IOException {
+        InputStreamReader IS = new InputStreamReader(System.in);
+        BufferedReader ob = new BufferedReader(IS);
+        System.out.println("Enter the number: ");
+        int number = Integer.parseInt(ob.readLine());
+        System.out.println("Factorial of " + number + " is :");
+        System.out.print(number + "!" + " =");
+        int Factorial = 1;
+        for (int i = 2; i <= number; i++) {
+            Factorial = Factorial * i;
         }
+        for (int i = 1; i <= number; i++) {
+            if (i == number) {
+                System.out.print(" " + i);
+            } else {
+                System.out.print(" " + i + " *");
+            }
+
+        }
+        System.out.println(" = " + Factorial);
+        System.out.println(" ");
+
     }
 }
